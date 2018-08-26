2c0cfcfd2acb502eb9d36417cdde17636b679b2f
Jaume Sabater <jsabater@gmail.com>
2018-08-26 22:14:52 +0200

    First working version of service.

--- services/get_login.py
+++ services/get_login.py
@@ -3,22 +3,29 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 from contextlib import closing
 from zato.server.service import Service
-# from genesisng import *
+from httplib import NO_CONTENT
+from genesisng.schema.login import Login
 
 class GetLogin(Service):
 
-    #class SimpleIO(object):
-    #    output_required = ('username', 'password', 'name', 'surname', 'email', 'is_admin')
+    class SimpleIO(object):
+        input_required = ('username')
+        output_optional = ('id', 'username', 'password', 'name', 'surname', 'email', 'is_admin')
 
     def handle(self):
-        self.logger.info('You just got your login. YAY!')
+        # TODO: Add this to a configuration file an use the value
+        # https://zato.io/docs/progguide/service-local-config.html
+        out_name = 'Genesis'
 
-        #out_name = 'Genesis'
-        #username = 'jsabater@gmail.com'
+        with closing(self.outgoing.sql.get(out_name).session()) as session:
+            result = session.query(Login).\
+                    filter(Login.username==self.request.input.username).\
+                    one_or_none()
 
-        #with closing(self.outgoing.sql.get(out_name).session()) as session:
-        #    result = session.query(login.Login).\
-        #            filter(login.Login.email==username).\
-        #            one()
+            if result:
+                self.logger.info('Retrieved login: {}' . format(result))
+                self.response.payload = result
+            else:
+                self.logger.info('Could not find a login with username: %s' % self.request.input.username)
+                self.response.status_code = NO_CONTENT
 
-        #    self.response.payload = result

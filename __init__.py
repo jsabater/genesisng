36f7fbca240d18b567a2f69ce6195013f09ca6f9
Jaume Sabater <jsabater@gmail.com>
2018-08-26 20:22:07 +0200

    Added __init__.py to root.

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+__version__ = '0.1.0'
+__author__ = 'Jaume Sabater <jsabater@gmail.com>'
+__all__ = ['schema']
+

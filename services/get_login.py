87d0201ec78df6a77a4e88fbb16577e1717c0ec6
Jaume Sabater <jsabater@gmail.com>
2018-08-26 20:21:57 +0200

    Commented a lot of code to have a very basic service with no SQL usage, but working.

--- services/get_login.py
+++ services/get_login.py
@@ -1,8 +1,9 @@
-# coding: utf8
+# -*- coding: utf-8 -*-
 
+from __future__ import absolute_import, division, print_function, unicode_literals
 from contextlib import closing
 from zato.server.service import Service
-from genesis-ng.schema.login import Login
+# from genesisng import *
 
 class GetLogin(Service):
 
@@ -10,15 +11,14 @@ class GetLogin(Service):
     #    output_required = ('username', 'password', 'name', 'surname', 'email', 'is_admin')
 
     def handle(self):
+        self.logger.info('You just got your login. YAY!')
 
-        out_name = 'Genesis'
-        username = 'jsabater@gmail.com'
+        #out_name = 'Genesis'
+        #username = 'jsabater@gmail.com'
 
-        with closing(self.outgoing.sql.get(out_name).session()) as session:
-            login = session.query(Login).\
-                    filter(Login.email==username).\
-                    one()
+        #with closing(self.outgoing.sql.get(out_name).session()) as session:
+        #    result = session.query(login.Login).\
+        #            filter(login.Login.email==username).\
+        #            one()
 
-            self.response.payload = login
-            # self.logger.info(login)
-
+        #    self.response.payload = result

de063190a03847c14f3314da16862619ff32e590
Jaume Sabater <jsabater@gmail.com>
2018-08-26 21:11:34 +0200

    Updated version.

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-__version__ = '0.1.0'
+__version__ = '0.1'
 __author__ = 'Jaume Sabater <jsabater@gmail.com>'
 __all__ = ['schema']
 

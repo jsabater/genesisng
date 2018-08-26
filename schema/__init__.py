fc7d0d37398ec52307d3b03bce85dc25199514b0
Jaume Sabater <jsabater@gmail.com>
2018-08-26 20:21:29 +0200

    Remove manual imports. Leave only __all_.

--- schema/__init__.py
+++ schema/__init__.py
@@ -1,13 +1,8 @@
 # coding: utf8
+
+__all__ = ['login', 'guest', 'room', 'rate', 'booking']
+
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
-
-from schema import login
-from schema import guest
-from schema import room
-from schema import rate
-from schema import booking
-
-__all__ = ['login', 'guest', 'room', 'rate', 'booking']
 

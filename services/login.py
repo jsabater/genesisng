0637cf7eee693681108517bd095a7bc0e05c73ec
Jaume Sabater <jsabater@gmail.com>
2018-08-28 00:47:19 +0200

    Added new login service.

--- services/login.py
+++ services/login.py
@@ -2,7 +2,7 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 from contextlib import closing
-from httplib import NO_CONTENT
+from httplib import OK, NO_CONTENT, CREATED
 from zato.server.service import Service
 from genesisng.schema.login import Login
 
@@ -24,7 +24,35 @@ class GetLogin(Service):
 
             if result:
                 self.logger.info('Retrieved login: {}' . format(result))
+                self.response.status_code = OK
                 self.response.payload = result
             else:
                 self.logger.info('Could not find a login with id: %s' % id)
                 self.response.status_code = NO_CONTENT
+
+class NewLogin(Service):
+
+    class SimpleIO(object):
+        input_required = ('username', 'password', 'name', 'surname', 'email')
+        input_optional = ('is_admin')
+        output_required = ('id', 'username', 'password', 'name', 'surname', 'email', 'is_admin')
+
+    def handle(self):
+        # TODO: Add this to a configuration file an use the value
+        # https://zato.io/docs/progguide/service-local-config.html
+        out_name = 'Genesis'
+        
+        # Create a Login instance and populate it with input parameters
+        i = self.request.input
+        l = Login(username=i.username, password=i.password, name=i.name, surname=i.surname, email=i.email)
+        # l.is_admin = i.get('is_admin')
+
+        self.logger.info('Creating a login: {}' . format(i))
+
+        with closing(self.outgoing.sql.get(out_name).session()) as session:
+            session.add(l)
+            session.commit()
+
+            self.response.status_code = CREATED
+            self.response.payload = l
+            self.response.headers['Location'] = '/genesisng/logins/%s' % l.id

a1359ab02815d2a2f21a98488f9855c6c46f9108
Jaume Sabater <jsabater@gmail.com>
2018-08-27 17:54:22 +0200

    Rearranged service so that there will be one verb per file.

--- services/login.py
+++ services/login.py
@@ -4,28 +4,27 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 from contextlib import closing
 from httplib import NO_CONTENT
 from zato.server.service import Service
-from sqlalchemy import or_
 from genesisng.schema.login import Login
 
-class LoginService(Service):
+class GetLogin(Service):
 
     class SimpleIO(object):
-        input_required = ('username')
+        input_required = ('id')
         output_optional = ('id', 'username', 'password', 'name', 'surname', 'email', 'is_admin')
 
-    def handle_GET(self):
+    def handle(self):
         # TODO: Add this to a configuration file an use the value
         # https://zato.io/docs/progguide/service-local-config.html
         out_name = 'Genesis'
-        u = self.request.input.username
-        self.logger.info('Checking for a login with username: %s' % u)
+        id = self.request.input.id
+        self.logger.info('Checking for a login with id: %s' % id)
 
         with closing(self.outgoing.sql.get(out_name).session()) as session:
-            result = session.query(Login).filter(or_(Login.username == u, Login.email == u)).one_or_none()
+            result = session.query(Login).filter(Login.id == id).one_or_none()
 
             if result:
                 self.logger.info('Retrieved login: {}' . format(result))
                 self.response.payload = result
             else:
-                self.logger.info('Could not find a login with username: %s' % u)
+                self.logger.info('Could not find a login with id: %s' % id)
                 self.response.status_code = NO_CONTENT

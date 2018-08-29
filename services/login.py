538b050cc4bca324ddad5974e517fa34f971fb96
Jaume Sabater <jsabater@gmail.com>
2018-08-29 09:10:18 +0200

    Added some comments to the code.

--- services/login.py
+++ services/login.py
@@ -7,12 +7,15 @@ from zato.server.service import Service
 from genesisng.schema.login import Login
 
 class Get(Service):
+    """Service class to get a login by id through channel /genesisng/logins/get/{id}."""
 
-    class SimpleIO(object):
+    class SimpleIO:
         input_required = ('id')
         output_optional = ('id', 'username', 'password', 'name', 'surname', 'email', 'is_admin')
 
     def handle(self):
+        """Returns a login given its id."""
+
         conn = self.kvdb.conn.get('genesisng:database:connection')
         id_ = self.request.input.id
         self.logger.info('Checking for a login with id: %s' % id_)
@@ -29,12 +32,15 @@ class Get(Service):
                 self.response.status_code = NO_CONTENT
 
 class List(Service):
+    """Service class to get a list of all logins in the system through channel /genesisng/logins/list."""
 
-    class SimpleIO(object):
+    class SimpleIO:
         input_required = ()
         output_optional = ('id', 'username', 'password', 'name', 'surname', 'email', 'is_admin')
 
     def handle(self):
+        """Returns a list of all logins in the system."""
+
         conn = self.kvdb.conn.get('genesisng:database:connection')
         self.logger.info('Getting a list of customers.')
 
@@ -47,12 +53,14 @@ class List(Service):
             self.response.payload[:] = output
 
 class Create(Service):
+    """Service class to create a new login through channel /genesisng/logins/create."""
 
-    class SimpleIO(object):
+    class SimpleIO:
         input_required = ('username', 'password', 'name', 'surname', 'email', 'is_admin')
         output_required = ('id', 'username', 'password', 'name', 'surname', 'email', 'is_admin')
 
     def handle(self):
+        """Creates a new login. All attributes are mandatory. Returns location header."""
         conn = self.kvdb.conn.get('genesisng:database:connection')
         
         # Create a Login instance and populate it with input parameters

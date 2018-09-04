fcb7d66e44177f4885372fb12145f75a27726290
Jaume Sabater <jsabater@gmail.com>
2018-09-04 21:08:19 +0200

    Added Trigram indexes and column indexes.

--- schema/login.py
+++ schema/login.py
@@ -1,24 +1,26 @@
 # coding: utf8
 from genesisng.schema import Base
 from sqlalchemy import Column, Boolean, Integer, String
-from sqlalchemy import UniqueConstraint
+from sqlalchemy import UniqueConstraint, Index
+from sqlalchemy.dialects import postgresql
 
 class Login(Base):
     __tablename__ = 'login'
     __rels__ = []
     __table_args__ = (
-        UniqueConstraint('username', 'email', name='login_username_email'),
+        Index('ix_trgm_login_username', 'username', postgresql_using='gin', postgresql_ops={'username': 'gin_trgm_ops'}),
+        Index('ix_trgm_login_name', 'name', postgresql_using='gin', postgresql_ops={'name': 'gin_trgm_ops'}),
+        Index('ix_trgm_login_surname', 'surname', postgresql_using='gin', postgresql_ops={'surname': 'gin_trgm_ops'}),
     )
 
     id = Column(Integer, primary_key=True)
-    username = Column(String(20))
+    username = Column(String(20), index=True, unique=True)
     password = Column(String(255))
     name = Column(String(50))
     surname = Column(String(50))
-    email = Column(String(255))
+    email = Column(String(255), index=True, unique=True)
     is_admin = Column(Boolean, default=False)
 
     def __repr__(self):
         return "<Login(id='%s', username='%s', name='%s', surname='%s', email='%s')>" % (
             self.id, self.username, self.name, self.surname, self.email)
-

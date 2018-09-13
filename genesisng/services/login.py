02d40fd1a09ebfd0cf21ada5068d59003b1dfb75
Jaume Sabater <jsabater@gmail.com>
2018-09-13 15:23:48 +0200

    Adapted format as per PEP 8. Removed unused imports.

--- genesisng/services/login.py
+++ genesisng/services/login.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, division
+from __future__ import print_function, unicode_literals
 from contextlib import closing
-from httplib import OK, NO_CONTENT, CREATED, NOT_FOUND, CONFLICT, BAD_REQUEST
+from httplib import OK, NO_CONTENT, CREATED, NOT_FOUND, CONFLICT
 from zato.server.service import Service, Boolean, Integer, AsIs
 from genesisng.schema.login import Login
 from sqlalchemy import or_, and_, func
 from sqlalchemy.exc import IntegrityError
 from urlparse import parse_qs
 
+
 class Get(Service):
-    """Service class to get a login by id through channel /genesisng/logins/get/{id}."""
+    """Service class to get a login by id."""
+    """Channel /genesisng/logins/get/{id}."""
 
     class SimpleIO:
         input_required = ('id')
-        output_optional = ('id', 'username', 'password', 'name', 'surname', 'email', 'is_admin')
+        output_optional = ('id', 'username', 'password', 'name', 'surname',
+                           'email', 'is_admin')
 
     def handle(self):
         conn = self.kvdb.conn.get('genesisng:database:connection')
@@ -29,12 +33,15 @@ class Get(Service):
                 self.response.status_code = NOT_FOUND
                 self.response.payload = ''
 
+
 class Validate(Service):
-    """Service class to validate credentials through channel /genesisng/logins/validate/{id}."""
+    """Service class to validate credentials."""
+    """Channel /genesisng/logins/validate."""
 
     class SimpleIO:
         input_required = ('username', AsIs('password'))
-        output_optional = ('id', 'username', 'password', 'name', 'surname', 'email', 'is_admin')
+        output_optional = ('id', 'username', 'password', 'name', 'surname',
+                           'email', 'is_admin')
 
     def handle(self):
         conn = self.kvdb.conn.get('genesisng:database:connection')
@@ -43,7 +50,8 @@ class Validate(Service):
 
         with closing(self.outgoing.sql.get(conn).session()) as session:
             result = session.query(Login).\
-                filter(and_(Login.username == username, Login.password == password)).\
+                filter(and_(Login.username == username,
+                            Login.password == password)).\
                 one_or_none()
 
             if result:
@@ -53,13 +61,16 @@ class Validate(Service):
                 self.response.status_code = NOT_FOUND
                 self.response.payload = ''
 
+
 class Create(Service):
-    """Service class to create a new login through channel /genesisng/logins/create."""
+    """Service class to create a new login."""
+    """Channel /genesisng/logins/create."""
 
     class SimpleIO:
         input_required = ('username', 'password', 'name', 'surname', 'email')
         input_optional = (Boolean('is_admin'))
-        output_optional = ('id', 'username', 'password', 'name', 'surname', 'email', 'is_admin')
+        output_optional = ('id', 'username', 'password', 'name', 'surname',
+                           'email', 'is_admin')
 
     def handle(self):
         # TODO: Use Cerberus to validate input?
@@ -67,19 +78,20 @@ class Create(Service):
         conn = self.kvdb.conn.get('genesisng:database:connection')
 
         p = self.request.input
-        l = Login(username=p.username, password=p.password, name=p.name, surname=p.surname, email=p.email)
-        l.is_admin = p.get('is_admin', False)
+        login = Login(username=p.username, password=p.password, name=p.name,
+                      surname=p.surname, email=p.email)
+        login.is_admin = p.get('is_admin', False)
 
         with closing(self.outgoing.sql.get(conn).session()) as session:
             try:
-                session.add(l)
+                session.add(login)
                 session.commit()
                 self.response.status_code = CREATED
-                self.response.payload = l
+                self.response.payload = login
                 url = self.kvdb.conn.get('genesisng:location:logins')
-                self.response.headers['Location'] = '%s/%s' % (url, l.id)
+                self.response.headers['Location'] = '%s/%s' % (url, login.id)
 
-            except IntegrityError as e:
+            except IntegrityError:
                 # Constraint prevents duplication of username or emails.
                 session.rollback()
                 self.response.status_code = CONFLICT
@@ -87,8 +99,10 @@ class Create(Service):
                 # https://medium.com/@suhas_chatekar/return-well-formed-error-responses-from-your-rest-apis-956b5275948
                 self.response.payload = ''
 
+
 class Delete(Service):
-    """Service class to delete an existing login through channel /genesisng/logins/delete/{id}"""
+    """Service class to delete an existing login."""
+    """Channel /genesisng/logins/delete/{id}."""
 
     class SimpleIO:
         input_required = (Integer('id'))
@@ -108,33 +122,40 @@ class Delete(Service):
                 self.response.status_code = NOT_FOUND
                 self.response.payload = ''
 
+
 class Update(Service):
-    """Service class to update an existing login through channel /genesisng/logins/update/{id}"""
+    """Service class to update an existing login."""
+    """Channel /genesisng/logins/update/{id}."""
 
     class SimpleIO:
         input_required = ('id')
-        input_optional = ('username', 'password', 'name', 'surname', 'email', 'is_admin')
-        # XXX: Should output_required be output_optional given we can return NOT_FOUND?
-        output_required = ('id', 'username', 'password', 'name', 'surname', 'email', 'is_admin')
+        input_optional = ('username', 'password', 'name', 'surname', 'email',
+                          'is_admin')
+        # XXX: Should output_required be output_optional given we can return
+        # NOT_FOUND?
+        output_required = ('id', 'username', 'password', 'name', 'surname',
+                           'email', 'is_admin')
         skip_empty_keys = True
 
     def handle(self):
         conn = self.kvdb.conn.get('genesisng:database:connection')
         id_ = self.request.input.id
         p = self.request.input
-        l = Login(id=id_, username=p.username, password=p.password, name=p.name, surname=p.surname, email=p.email, is_admin=p.is_admin)
+        login = Login(id=id_, username=p.username, password=p.password,
+                      name=p.name, surname=p.surname, email=p.email,
+                      is_admin=p.is_admin)
 
         with closing(self.outgoing.sql.get(conn).session()) as session:
             result = session.query(Login).filter(Login.id == id_).one_or_none()
 
             if result:
                 # Update dictionary keys
-                result.username = l.username
-                result.password = l.password
-                result.name = l.name
-                result.surname = l.surname
-                result.email = l.email
-                result.is_admin = l.is_admin
+                result.username = login.username
+                result.password = login.password
+                result.name = login.name
+                result.surname = login.surname
+                result.email = login.email
+                result.is_admin = login.is_admin
                 session.commit()
                 self.response.status_code = OK
                 self.response.payload = result
@@ -142,19 +163,25 @@ class Update(Service):
                 self.response.status_code = NOT_FOUND
                 self.response.payload = ''
 
+
 class List(Service):
-    """Service class to get a list of all logins in the system through channel /genesisng/logins/list."""
+    """Service class to get a list of all logins in the system."""
+    """Channel /genesisng/logins/list."""
 
     class SimpleIO:
-        input_optional = ('page', 'size', 'sort_by', 'order_by', 'filters', 'search', 'fields')
-        output_optional = ('id', 'username', 'password', 'name', 'surname', 'email', 'is_admin')
+        input_optional = ('page', 'size', 'sort_by', 'order_by', 'filters',
+                          'search', 'fields')
+        output_optional = ('id', 'username', 'password', 'name', 'surname',
+                           'email', 'is_admin')
         output_repeated = True
         skip_empty_keys = True
 
     def handle(self):
         conn = self.kvdb.conn.get('genesisng:database:connection')
-        default_page_size = int(self.kvdb.conn.get('genesisng:database:default_page_size'))
-        max_page_size = int(self.kvdb.conn.get('genesisng:database:max_page_size'))
+        default_page_size = int(self.kvdb.conn.
+                                get('genesisng:database:default_page_size'))
+        max_page_size = int(self.kvdb.conn.get(
+            'genesisng:database:max_page_size'))
         # TODO: Have a default order_by and sort_by in the KVDB?
         default_order_by = 'id'
         default_sort_by = 'asc'
@@ -197,12 +224,15 @@ class List(Service):
             # Handle sorting
             try:
                 order_by = qs['order_by'][0].lower()
-                # Fields allowed for ordering are id, username, name, surname and email
-                if order_by not in ('id', 'username', 'name', 'surname', 'email'):
+                # Fields allowed for ordering are id, username, name, surname
+                # and email
+                if order_by not in ('id', 'username', 'name', 'surname',
+                                    'email'):
                     order_by = default_order_by
 
                 sort_by = qs['sort_by'][0].lower()
-                sort_by = default_sort_by if sort_by not in ('asc', 'desc') else sort_by
+                sort_by = default_sort_by if sort_by not in (
+                    'asc', 'desc') else sort_by
             except (ValueError, KeyError, IndexError):
                 # Assume default values instead of returning 400 Bad Request
                 pass
@@ -211,13 +241,18 @@ class List(Service):
             try:
                 for f in qs['filters']:
                     field, operator, value = f.split('|')
-                    if field not in ('id', 'username', 'password', 'name', 'surname', 'email', 'is_admin'):
-                        raise ValueError('Field %s is not allowed for filtering' % field)
+                    if field not in ('id', 'username', 'password', 'name',
+                                     'surname', 'email', 'is_admin'):
+                        raise ValueError(
+                            'Field %s is not allowed for filtering' % field)
                     if operator not in ('lt', 'lte', 'eq', 'ne', 'gte', 'gt'):
-                        raise ValueError('Operator %s is not allowed for filtering' % operator)
+                        raise ValueError(
+                            'Operator %s is not allowed for filtering' %
+                            operator)
                     filters.append((field, operator, value))
             except (ValueError, KeyError):
-                # Do not apply any filtering instead of returning 400 Bad Request
+                # Do not apply any filtering instead of returning 400 Bad
+                # Request
                 pass
 
             # Handle search
@@ -230,11 +265,14 @@ class List(Service):
             # Handle fields projection
             try:
                 for field in qs['fields']:
-                    if field not in ('id', 'username', 'password', 'name', 'surname', 'email', 'is_admin'):
-                        raise ValueError('Field %s is not allowed for projection' % field)
+                    if field not in ('id', 'username', 'password', 'name',
+                                     'surname', 'email', 'is_admin'):
+                        raise ValueError(
+                            'Field %s is not allowed for projection' % field)
                     fields.append(field)
             except (ValueError, KeyError):
-                # Do not apply any fields projection instead of returning 400 Bad Request
+                # Do not apply any fields projection instead of returning 400
+                # Bad Request
                 pass
 
         # Calculate limit and offset
@@ -271,8 +309,9 @@ class List(Service):
         # Prepare fields projection
         columns = []
         if not fields:
-            fields = ('id', 'username', 'password', 'name', 'surname', 'email', 'is_admin')
-        columns = [Login.__table__.columns[field] for field in fields]
+            fields = ('id', 'username', 'password', 'name',
+                      'surname', 'email', 'is_admin')
+        columns = [Login.__table__.columns[f] for f in fields]
 
         # Execute query
         with closing(self.outgoing.sql.get(conn).session()) as session:
@@ -291,7 +330,8 @@ class List(Service):
             if direction == 'asc':
                 query = query.order_by(Login.__table__.columns[criteria].asc())
             else:
-                query = query.order_by(Login.__table__.columns[criteria].desc())
+                query = query.order_by(
+                    Login.__table__.columns[criteria].desc())
             query = query.offset(offset)
             query = query.limit(limit)
             result = query.all()

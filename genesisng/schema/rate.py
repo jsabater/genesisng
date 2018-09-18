f5ec538d4683b8941849825b552e07470094d4eb
Jaume Sabater <jsabater@gmail.com>
2018-09-18 08:50:41 +0200

    Reformat according to PEP8. Delete unused attribute.

--- genesisng/schema/rate.py
+++ genesisng/schema/rate.py
@@ -2,21 +2,24 @@
 from genesisng.schema import Base
 from sqlalchemy import Column, Boolean, Integer, Float, Date
 from sqlalchemy import UniqueConstraint, CheckConstraint
-from sqlalchemy.dialects.postgresql import ExcludeConstraint, DATERANGE, TSRANGE
+from sqlalchemy.dialects.postgresql import ExcludeConstraint
 from sqlalchemy.ext.hybrid import hybrid_property
 from psycopg2.extras import DateRange
 from sqlalchemy.sql.elements import quoted_name
+
 
 class Rate(Base):
     __tablename__ = 'rate'
     __rels__ = []
     __table_args__ = (
-        UniqueConstraint('date_from', 'date_to', name='rate_date_from_date_to'),
+        UniqueConstraint('date_from', 'date_to',
+                         name='rate_date_from_date_to'),
         CheckConstraint('date_from < date_to'),
         # Prevent dates from overlapping by using an exclusion constraint and
         # the overlap operator (&&) for the daterange type
         ExcludeConstraint(
-            (Column(quoted_name('daterange(date_from, date_to)', quote=False)), '&&'),
+            (Column(quoted_name('daterange(date_from, date_to)',
+                                quote=False)), '&&'),
             using='gist', name='rate_date_range'
         ),
     )
@@ -26,7 +29,6 @@ class Rate(Base):
     id = Column(Integer, primary_key=True)
     date_from = Column(Date)
     date_to = Column(Date)
-    date_range = Column(DATERANGE())
     base_price = Column(Float)
     bed_price = Column(Float)
     published = Column(Boolean, default=False)
@@ -42,4 +44,3 @@ class Rate(Base):
     @hybrid_property
     def date_range(self):
         return DateRange(self.date_from, self.date_to)
-

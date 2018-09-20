37f89e6330c58fb65441cae34d170f6c8b326cb9
Jaume Sabater <jsabater@gmail.com>
2018-09-20 22:36:39 +0200

    Adapted to PEP8 requirements.

--- genesisng/__init__.py
+++ genesisng/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
-
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, division
+from __future__ import print_function, unicode_literals
+from . import schema
 
 __version__ = '0.1'
 __author__ = 'Jaume Sabater <jsabater@gmail.com>'
 __all__ = ['schema']
-

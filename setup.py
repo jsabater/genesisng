af932cff30d012b0d06c1887ca8f506277c93bd3
Jaume Sabater <jsabater@gmail.com>
2018-10-09 21:46:01 +0200

    Added more requirements, package data and long description.

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
-
+from __future__ import absolute_import, division
+from __future__ import print_function, unicode_literals
 from setuptools import setup
 
 setup(name='genesisng',
       version='0.1',
-      description='Example package on how to use SQLAlchemy with Zato',
+      description='Genesis Next Generation',
+      long_description='Example package on how to use SQLAlchemy with Zato',
       url='https://bitbucket.org/jsabater/genesisng',
       author='Jaume Sabater',
       author_email='jsabater@gmail.com',
       license='MIT',
+      keywords='sqlalchemy zato api services',
       packages=['genesisng'],
-      install_requires=['configparser', 'psycopg2', 'sqlalchemy']
+      install_requires=[
+        'configparser',
+        'hashids',
+        'psycopg2',
+        'sqlalchemy'
+      ],
+      include_package_data=True,
       zip_safe=False)
-

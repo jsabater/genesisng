3b6c2a0f617cd9a32ecfb6f55989002f2ebb185a
Jaume Sabater <jsabater@gmail.com>
2018-10-10 23:21:33 +0200

    Added guests/{id}/details test.

--- tests.sh
+++ tests.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Logins
 curl -v -g "http://127.0.0.1:11223/genesisng/logins/1/get"; echo ""
 curl -v -g -XPOST -d '{"username": "jsabater", "password": "123456"}' "http://127.0.0.1:11223/genesisng/logins/validate"; echo ""
 curl -v -g -XPOST -d '{"username": "bcamelas", "password": "123456", "name": "Benito", "email": "bcamelas@gmail.com"}' "http://127.0.0.1:11223/genesisng/logins/create"; echo ""
@@ -11,3 +12,5 @@ curl -v -g "http://127.0.0.1:11223/genesisng/logins/list?page=1&size=50&sort_by=
 curl -v -g "http://127.0.0.1:11223/genesisng/logins/list?page=1&size=50&sort_by=name&order_by=desc&fields=id&fields=name&filters=id|gt|50"; echo ""
 curl -v -g "http://127.0.0.1:11223/genesisng/logins/list?page=1&size=50&sort_by=name&order_by=desc&search=Black"; echo ""
 
+# Guests
+curl -v -g "http://127.0.0.1:11223/genesisng/guests/1/get"; echo ""

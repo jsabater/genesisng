2f077af6484aeed564d3121ac183ff89ee97ab30
Jaume Sabater <jsabater@gmail.com>
2018-10-15 23:27:11 +0200

    Added commented lines for future use of dictalchemy.

--- genesisng/schema/base.py
+++ genesisng/schema/base.py
@@ -1,7 +1,9 @@
 # coding: utf8
 from __future__ import absolute_import, division
 from __future__ import print_function, unicode_literals
+# from dictalchemy import DictableModel
 from sqlalchemy.ext.declarative import declarative_base
 
 
 Base = declarative_base()
+# Base = declarative_base(cls=DictableModel)

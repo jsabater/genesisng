0b988fe611ad3db97391b18571a9ebf6afde9ab0
Jaume Sabater <jsabater@gmail.com>
2018-10-23 16:29:10 +0200

    Added blank lines.

--- genesisng/__init__.py
+++ genesisng/__init__.py
@@ -2,5 +2,7 @@
 from __future__ import absolute_import, division
 from __future__ import print_function, unicode_literals
 
+
 __version__ = '0.1'
 __author__ = 'Jaume Sabater <jsabater@gmail.com>'
+

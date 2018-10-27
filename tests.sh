5402dd2208f72abf92acd07746c1c328e428547d
Jaume Sabater <jsabater@gmail.com>
2018-10-27 16:54:16 +0200

    Updated to make use of recently modified parameters on list services.

--- tests.sh
+++ tests.sh
@@ -21,16 +21,22 @@ curl -v -g -XPOST -d '{"username": "mjordan", "password": "123456", "name": "Mic
 curl -v -g "http://127.0.0.1:11223/genesisng/logins/list"; echo ""
 
 # List using page and size
-curl -v -g "http://127.0.0.1:11223/genesisng/logins/list?page=2&size=50"; echo ""
+curl -v -g "http://127.0.0.1:11223/genesisng/logins/list?page=2&size=20"; echo ""
 
-# List using page, size, sort_by, order_by and fields
-curl -v -g "http://127.0.0.1:11223/genesisng/logins/list?page=1&size=50&sort_by=name&order_by=desc&fields=id&fields=name"; echo ""
+# List using page, size and sort
+curl -v -g "http://127.0.0.1:11223/genesisng/logins/list?page=2&size=20&sort=name|desc"; echo ""
 
-# List using page, size, sort_by, order_by, fields and filters
-curl -v -g "http://127.0.0.1:11223/genesisng/logins/list?page=4&size=40&sort_by=name&order_by=desc&fields=id&fields=name&filters=id|gt|50"; echo ""
+# List using page, size, sort and fields
+curl -v -g "http://127.0.0.1:11223/genesisng/logins/list?page=2&size=20&sort=name|desc&fields=id&fields=name"; echo ""
 
-# List using page, size, sort_by, order_by and search
-curl -v -g "http://127.0.0.1:11223/genesisng/logins/list?page=1&size=50&sort_by=name&order_by=desc&search=Black"; echo ""
+# List using page, size, sort, fields and filters
+curl -v -g "http://127.0.0.1:11223/genesisng/logins/list?page=2&size=20&sort=name|desc&fields=id&fields=name&filters=id|gt|50"; echo ""
+
+# List using sort and multiple filters
+curl -v -g "http://127.0.0.1:11223/genesisng/logins/list?sort=name|desc&filters=id|gt|50&filters=id|lt|500&operator=and"; echo ""
+
+# List using sort and search
+curl -v -g "http://127.0.0.1:11223/genesisng/logins/list?sort=name|desc&search=Black"; echo ""
 
 # Guests
 
@@ -56,13 +62,13 @@ curl -v -g "http://127.0.0.1:11223/genesisng/guests/list"; echo ""
 curl -v -g "http://127.0.0.1:11223/genesisng/guests/list?page=3&size=35"; echo ""
 
 # List using page, size, sort_by, order_by and fields
-curl -v -g "http://127.0.0.1:11223/genesisng/guests/list?page=10&size=20&sort_by=surname&order_by=asc&fields=id&fields=name&fields=surname"; echo ""
+curl -v -g "http://127.0.0.1:11223/genesisng/guests/list?page=10&size=20&sort=surname|asc&fields=id&fields=name&fields=surname"; echo ""
 
 # List using page, size, sort_by, order_by, fields and filters
-curl -v -g "http://127.0.0.1:11223/genesisng/guests/list?page=10&size=10&sort_by=name&order_by=desc&fields=id&fields=name&fields=surname&fields=email&filters=birthdate|gte|1990-01-01"; echo ""
+curl -v -g "http://127.0.0.1:11223/genesisng/guests/list?page=10&size=10&sort=name|desc&fields=id&fields=name&fields=surname&fields=email&filters=birthdate|gte|1990-01-01"; echo ""
 
 # List using page, size, sort_by, order_by and search
-curl -v -g "http://127.0.0.1:11223/genesisng/guests/list?page=4&size=10&sort_by=country&order_by=asc&search=Palma"; echo ""
+curl -v -g "http://127.0.0.1:11223/genesisng/guests/list?page=4&size=10&sort=country|asc&search=Palma"; echo ""
 
 # Bookings from a guest
 curl -v -g "http://127.0.0.1:11223/genesisng/guests/1/bookings"; echo ""

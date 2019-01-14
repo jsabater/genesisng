7e7cf2c8b01efea0a8b082031dbe12ce960d7aa9
Jaume Sabater <jsabater@gmail.com>
2019-01-14 13:42:13 +0100

    Services sub-directory is now a package.

---
+++ genesisng/services/__init__.py
@@ -0,0 +1,12 @@
+# coding: utf8
+from __future__ import absolute_import, division
+from __future__ import print_function, unicode_literals
+from . import booking
+from . import guest
+from . import login
+from . import rate
+from . import room
+
+
+__all__ = ['booking', 'guest', 'login', 'rate', 'room']
+

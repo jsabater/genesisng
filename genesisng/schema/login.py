7c2f999350d9133f4f5c49abdf74b8af673b367f
Jaume Sabater <jsabater@gmail.com>
2019-01-14 13:33:37 +0100

    Added hashed password and documentation.

--- genesisng/schema/login.py
+++ genesisng/schema/login.py
@@ -2,10 +2,39 @@
 from __future__ import absolute_import, division
 from __future__ import print_function, unicode_literals
 from .base import Base
-from sqlalchemy import Column, Boolean, Integer, String, Index
+from sqlalchemy import Column, Boolean, Integer, String, Index, func
+from sqlalchemy.orm import deferred
+from sqlalchemy.ext.hybrid import hybrid_property, Comparator
+from passlib.hash import bcrypt
+
+
+class PasswordComparator(Comparator):
+    """Comparator class used to hash the clear-text password sent to the
+    database and verify it against the stored value."""
+    def __eq__(self, other):
+        return self.__clause_element__() == \
+            func.crypt(other, self.__clause_element__())
 
 
 class Login(Base):
+    """
+    Model class to represent a login to the system.
+
+    Includes trigram GIN indexes for pattern-matching searches on fields
+    `username`, `name` and `surname` and B-Tree indexes to compare, sort and
+    reduce memory consumption on fields `username`, `name`, `surname` and
+    `email`.
+
+    Uses the BCrypt algorithm from the `passlib`_ library (which also uses
+    `bcrypt`_) to hash passwords. When adding new records, passwords are hashed
+    before being sent to the database. When validating, a custom comparator is
+    used to send passwords to the database, which uses its cryptographic
+    functions to verify it.
+
+    .. _passlib: https://pypi.org/project/passlib/
+    .. _bcrypt: https://pypi.org/project/bcrypt/
+    """
+
     __tablename__ = 'login'
     __rels__ = []
     __table_args__ = (
@@ -18,20 +47,35 @@ class Login(Base):
               postgresql_ops={'surname': 'gin_trgm_ops'}),
     )
 
-    # SQLAlchemy automatically creates the table column using the SERIAL type
-    # which triggers the creation of a sequence automatically.
-    # Username and e-mail must be unique.
-    # B-tree indexes on sorting fields to speed up operations and reduce memory
-    # consumption.
-    # https://www.postgresql.org/docs/current/static/indexes-ordering.html
     id = Column(Integer, primary_key=True)
+    """Primary key. Autoincrementing integer."""
     username = Column(String(20), index=True, unique=True, nullable=False)
-    password = Column(String(255), nullable=False)
+    """Username or alias of the person. Must be unique."""
+    _password = deferred(Column('password', String(255), nullable=False))
     name = Column(String(50), index=True)
+    """First name of the person."""
     surname = Column(String(50), index=True)
+    """Last name of the person."""
     email = Column(String(255), index=True, unique=True)
+    """Electronic mail address. Must be unique."""
     is_admin = Column(Boolean, default=False)
+    """Is this person an administrator?"""
+
+    @hybrid_property
+    def password(self):
+        """Hashed password. Uses `deferred()` to prevent the field from being
+        fetched by default when an object is loaded from the database."""
+        return self._password
+
+    @password.comparator
+    def password(cls):
+        return PasswordComparator(cls._password)
+
+    @password.setter
+    def password(self, plaintext_password):
+        self._password = bcrypt.hash(plaintext_password)
 
     def __repr__(self):
+        """String representation of the object."""
         return "<Login(id='%s', username='%s', name='%s', surname='%s', email='%s')>" % (
             self.id, self.username, self.name, self.surname, self.email)

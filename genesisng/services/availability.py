dfaf191301e2a6aeb28c4b4e4c1fe8344c773b91
Jaume Sabater <jsabater@gmail.com>
2019-02-13 11:07:03 +0100

    Added first, working version of the availability service.

---
+++ genesisng/services/availability.py
@@ -0,0 +1,151 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division
+from __future__ import print_function, unicode_literals
+from contextlib import closing
+from httplib import OK, NO_CONTENT, BAD_REQUEST
+from zato.server.service import Service
+from zato.server.service import Integer, Date, List
+from genesisng.schema.room import Room
+from genesisng.schema.rate import Rate
+from genesisng.schema.booking import Booking
+from sqlalchemy import func, tuple_, case, cast
+from sqlalchemy import Integer as sqlInteger
+from sqlalchemy import Float as sqlFloat
+from sqlalchemy import Date as sqlDate
+
+
+class Search(Service):
+    """
+    Service class to search for availability.
+
+    Channel ``/genesisng/availability/search``.
+
+    Uses `SimpleIO`_.
+
+    Stores the search results in the ``availability`` cache. Returns
+    ``Cache-Control``, ``Last-Modified`` and ``ETag`` headers. Returns a
+    ``Content-Language`` header.
+
+    Returns ``OK`` if results have been found, ``NO_CONTENT`` if there is no
+    availability or ``BAD_REQUEST`` if the check-in date is not before the
+    check-out date and there is at least 1 day in between.
+    """
+
+    class SimpleIO(object):
+        input_required = (Date('check_in'), Date('check_out'),
+                          Integer('guests'))
+        input_optional = (List('rooms'))
+        output_optional = ('id', 'number', 'name', 'sbl_beds', 'dbl_beds',
+                           'accommodates', 'code', 'nights', 'total_price')
+        skip_empty_keys = True
+        output_repeated = True
+
+    def handle(self):
+        """
+        Service handler.
+
+        :param guests: The number of guests
+        :type guests: int
+        :param check_in: The date the guests want to arrive.
+        :type check_in: date
+        :param check_out: The date the guests want to leave.
+        :type check_out: date
+        :param rooms: A list of room ids to filter the results
+        :type rooms: list
+
+        :returns: All attributes of a
+            :class:`~genesisng.schema.availability.Result` model class.
+        :rtype: dict
+        """
+
+        conn = self.user_config.genesisng.database.connection
+        check_in = self.request.input.check_in
+        check_out = self.request.input.check_out
+        guests = self.request.input.guests
+
+        # Check dates
+        if check_in >= check_out:
+            self.response.status_code = BAD_REQUEST
+            return
+
+        # Process optional list of room ids
+        try:
+            rooms = list(map(
+                int,
+                self.request.input.rooms)) if self.request.input.rooms else []
+        except ValueError:
+            rooms = []
+
+        # Otherwise, retrieve the data
+        with closing(self.outgoing.sql.get(conn).session()) as session:
+
+            # Sum of nights and prices per season (0..N)
+            p = session.query(
+                func.SUM(
+                    case(
+                        [(check_out > Rate.date_to, Rate.date_to)],
+                        else_=check_out
+                    ) -
+                    case(
+                        [(check_in > Rate.date_from, check_in)],
+                        else_=Rate.date_from
+                    )
+                ).label('nights'),
+                (func.SUM((
+                    case(
+                        [(check_out > Rate.date_to, Rate.date_to)],
+                        else_=check_out
+                    ) -
+                    case(
+                        [(check_in > Rate.date_from, check_in)],
+                        else_=Rate.date_from
+                    )) * (Rate.base_price + Rate.bed_price * guests)
+                ).label('price'))
+                ).\
+                filter(
+                    tuple_(Rate.date_from, Rate.date_to).
+                    op('OVERLAPS')
+                    (tuple_(cast(check_in, sqlDate), cast(check_out, sqlDate)))
+                ).\
+                filter(Rate.published.is_(True)).\
+                cte(name='p')
+
+            # Room availability using a sub-select
+            subq = session.query(Booking.id_room.label('id')).\
+                filter(
+                    tuple_(Booking.check_in, Booking.check_out).
+                    op('OVERLAPS')
+                    (tuple_(cast(check_in, sqlDate), cast(check_out, sqlDate)))
+                ).\
+                filter(Booking.cancelled.is_(None)).\
+                subquery('subq')
+
+            a = session.query(Room.id, Room.floor_no, Room.room_no, Room.name,
+                              Room.sgl_beds, Room.dbl_beds,  Room.supplement,
+                              Room.code, Room.number, Room.accommodates).\
+                filter(Room.deleted.is_(None)).\
+                filter(Room.id.notin_(subq)).\
+                filter(Room.accommodates >= guests)
+            if rooms:
+                a = a.filter(Room.id.any(rooms))
+            a = a.cte(name='a')
+
+            result = session.query(
+                a.c.id, a.c.floor_no, a.c.room_no, a.c.name, a.c.sgl_beds,
+                a.c.dbl_beds, a.c.code, a.c.number, a.c.accommodates,
+                cast(p.c.nights, sqlInteger).label('nights'),
+                cast(a.c.supplement * p.c.nights + p.c.price, sqlFloat).
+                label('total_price')).\
+                order_by('total_price ASC').\
+                order_by(a.c.accommodates.asc()).\
+                order_by(a.c.sgl_beds.asc()).\
+                order_by(a.c.dbl_beds.asc()).\
+                order_by(a.c.floor_no.asc()).\
+                order_by(a.c.room_no.asc()).\
+                all()
+
+            if result:
+                self.response.payload[:] = result
+                self.response.status_code = OK
+            else:
+                self.response.status_code = NO_CONTENT
---
+++ genesisng/sql/schema_availability.sql
@@ -0,0 +1,71 @@
+-- Function that emulates Transact-SQL's IIF (if-and-only-if)
+CREATE OR REPLACE FUNCTION IIF(BOOLEAN, DATE, DATE) RETURNS DATE
+AS $$
+    SELECT CASE $1 WHEN True THEN $2 ELSE $3 END
+$$
+LANGUAGE SQL IMMUTABLE;
+
+-- Function to have together all steps that lead to availability and pricing calculation
+CREATE OR REPLACE FUNCTION availability(check_in DATE, check_out DATE, guests INTEGER, room INTEGER[] DEFAULT '{}')
+RETURNS TABLE (
+    r_id INTEGER,
+    r_floor_no INTEGER,
+    r_room_no INTEGER,
+    r_name VARCHAR,
+    r_sgl_beds INTEGER,
+    r_dbl_beds INTEGER,
+    r_accommodates INTEGER,
+    r_code VARCHAR,
+    t_nights INTEGER,
+    t_price REAL
+) AS $$
+BEGIN
+RETURN QUERY
+(
+    WITH p AS (
+         -- Sum of nights and prices per season (0..N)
+         SELECT SUM(IIF($2 > t.date_to, t.date_to, $2) - IIF($1 > t.date_from, $1, t.date_from)) AS nights,
+                SUM(
+                    (IIF($2 > t.date_to, t.date_to, $2) - IIF($1 > t.date_from, $1, t.date_from)) *
+                    (t.base_price + t.bed_price * $3)
+                ) AS price
+           FROM rate AS t
+          WHERE (t.date_from, t.date_to) OVERLAPS ($1, $2)
+            AND t.published = True
+         ),
+         a AS (
+         -- Room availability
+         SELECT r.id AS r_id,
+                r.floor_no AS r_floor_no,
+                r.room_no AS r_room_no,
+                r.name AS r_name,
+                r.sgl_beds AS r_sgl_beds,
+                r.dbl_beds AS r_dbl_beds,
+                (r.sgl_beds + r.dbl_beds * 2) AS r_accommodates,
+                r.supplement AS r_supplement,
+                r.code AS r_code
+           FROM room AS r
+          WHERE r.id NOT IN (
+                SELECT b.id_room
+                  FROM booking as b
+                 WHERE (b.check_in, b.check_out) OVERLAPS ($1, $2)
+                   AND b.cancelled IS NULL
+                )
+            AND (r.sgl_beds + r.dbl_beds * 2) >= $3
+            AND CASE WHEN $4 = '{}'::INTEGER[] THEN r.id > 0 ELSE r.id = ANY($4) END
+         )
+  SELECT a.r_id AS r_id,
+         a.r_floor_no AS r_floor_no,
+         a.r_room_no AS r_room_no,
+         a.r_name AS r_name,
+         a.r_sgl_beds AS r_sgl_beds,
+         a.r_dbl_beds AS r_dbl_beds,
+         a.r_accommodates AS r_accommodates,
+         a.r_code AS r_code,
+         p.nights::INTEGER AS t_nights,
+         (a.r_supplement * p.nights + p.price)::REAL AS t_price
+    FROM a, p
+ORDER BY t_price ASC, r_accommodates ASC, r_sgl_beds ASC, r_dbl_beds ASC, r_floor_no ASC, r_room_no ASC
+);
+END
+$$ LANGUAGE plpgsql;

775f29ae888d3415be29e7b5e9f340b2bf8b9494
Jaume Sabater <jsabater@gmail.com>
2019-03-03 21:35:08 +0100

    Added some more tests to guests, rooms and availability.

--- tests.sh
+++ tests.sh
@@ -38,6 +38,7 @@ curl -v -g "http://127.0.0.1:11223/genesisng/logins/list?sort=name|desc&filters=
 # List using sort and search
 curl -v -g "http://127.0.0.1:11223/genesisng/logins/list?sort=name|desc&search=Black"; echo ""
 
+
 # Guests
 
 # Get
@@ -54,6 +55,10 @@ curl -v -g -XPOST -d '{"name": "Michelle", "surname": "Pfeiffer", "gender": "Fem
 
 # Update
 curl -v -g -XPOST -d '{"name": "Angelina", "surname": "Jolie", "gender": "Female", "email": "ajolie@gmail.com", "birthdate": "1975-06-04", "address1": "1419 Westwood Blvd", "address2": "Westwood", "locality": "Los Angeles", "postcode": "3H35+W8", "province": "California", "country": "US", "mobile_phone": "+1.5417543010"}' "http://127.0.0.1:11223/genesisng/guests/2/update"; echo ""
+
+# Upsert
+curl -v -g -XPOST -d '{"name": "Diane", "surname": "Heidkrüger", "gender": "Female", "email": "dkruger@gmail.com"}' "http://127.0.0.1:11223/genesisng/guests/upsert"; echo ""
+curl -v -g -XPOST -d '{"name": "Diane", "surname": "Heidkrüger", "gender": "Female", "email": "dkruger@gmail.com", "birthdate": "1976-07-15", "address1": "St. Matthäus Catholic Church", "address2": "", "locality": "Algermissen", "postcode": "31191", "province": "Lower Saxony", "country": "DE", "mobile_phone": "+49.1511234567"}' "http://127.0.0.1:11223/genesisng/guests/upsert"; echo ""
 
 # List
 curl -v -g "http://127.0.0.1:11223/genesisng/guests/list"; echo ""
@@ -72,3 +77,33 @@ curl -v -g "http://127.0.0.1:11223/genesisng/guests/list?page=4&size=10&sort=cou
 
 # Bookings from a guest
 curl -v -g "http://127.0.0.1:11223/genesisng/guests/1/bookings"; echo ""
+
+
+# Rooms
+
+# Get
+curl -v -g "http://127.0.0.1:11223/genesisng/rooms/1/get"; echo ""
+
+# Delete
+curl -v -g "http://127.0.0.1:11223/genesisng/rooms/1/delete"; echo ""
+
+# Restore
+curl -v -g "http://127.0.0.1:11223/genesisng/rooms/1/restore"; echo ""
+
+# Create
+curl -v -g -XPOST -d '{"floor_no": 1, "room_no": 1, "name": "Cosy room with views to the sea", "sgl_beds": 0, "dbl_beds": 1, "supplement": 25}' "http://127.0.0.1:11223/genesisng/rooms/create"; echo ""
+
+
+
+# Availability
+
+# Extras
+curl -v -g "http://127.0.0.1:11223/genesisng/availability/extras"; echo ""
+
+# Search
+curl -v -g "http://127.0.0.1:11223/genesisng/availability/search?guests=3&check_in=2017-07-01&check_out=2017-07-10"; echo ""
+curl -v -g "http://127.0.0.1:11223/genesisng/availability/search?guests=3&check_in=2017-07-01&check_out=2017-07-10&rooms=3&rooms=6"; echo ""
+
+# Bookings
+
+curl -v -g -XPOST -d '{"id_guest": 1, "id_room"}' "http://127.0.0.1:11223/genesisng/bookings/create"; echo ""

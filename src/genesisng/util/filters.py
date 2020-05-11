5da8251e50943d8ee34255fed9b5e3023e871ed3
Jaume Sabater <jsabater@gmail.com>
2020-05-11 22:56:24 +0200

    Updated documentation and it now makes use of lambda functions to reduce code extent.

--- src/genesisng/util/filters.py
+++ src/genesisng/util/filters.py
@@ -2,57 +2,45 @@
 from sqlalchemy import or_, and_
 
 
-def parse_filters(filters, operator, cols, query):
+def parse_filters(filters, nexus, cols, query):
     """
-    Converts filters received through query string, which have already been
+    Converts the filters received through query string, which have already been
     parsed by `parse_args`, into conditions for the Query object of SQLAlchemy.
 
     :param filters: A list of filters, each with field, operator and value.
     :type filters: List of tuples
 
-    :param operator: The operator to compare the field with the value.
-    :type operator: String
+    :param nexus: The operator to compare the field with the value.
+    :type nexus: String
 
     :param cols: A list of the attributes of the entity as table columns.
-    :type cols: sqlalchemy.sql.base.ImmutableColumnCollection
+    :type cols: :class:`~sqlalchemy:sqlalchemy.sql.base.ImmutableColumnCollection`
 
     :param query: The SQLAlchemy Query object being constructed.
-    :type query: sqlalchemy.orm.query.Query
+    :type query: :class:`~sqlalchemy:sqlalchemy.orm.query.Query`
 
     :returns: A modified Query object that include filters.
-    :rtype: sqlalchemy.orm.query.Query
+    :rtype: :class:`~sqlalchemy:sqlalchemy.orm.query.Query`
     """
 
+    # List of valid operators as lambda funcions
     OPERATORS = {
-        'eq': lambda f, a: f == a,
-        'ne': lambda f, a: f != a,
-        'gt': lambda f, a: f > a,
-        'lt': lambda f, a: f < a,
-        'gte': lambda f, a: f >= a,
-        'lte': lambda f, a: f <= a
+        'eq': lambda f, v: f == v,
+        'ne': lambda f, v: f != v,
+        'gt': lambda f, v: f > v,
+        'lt': lambda f, v: f < v,
+        'gte': lambda f, v: f >= v,
+        'lte': lambda f, v: f <= v
     }
 
     # Process filters
     clauses = []
     for f in filters:
         field, operator, value = f
-        # TODO: Turn the follwing if..else block into one sentence
-        # clauses.append(cols[field] OPERATORS[operator] value)
-        if operator == 'lt':
-            clauses.append(cols[field] < value)
-        elif operator == 'lte':
-            clauses.append(cols[field] <= value)
-        elif operator == 'eq':
-            clauses.append(cols[field] == value)
-        elif operator == 'ne':
-            clauses.append(cols[field] != value)
-        elif operator == 'gte':
-            clauses.append(cols[field] >= value)
-        elif operator == 'gt':
-            clauses.append(cols[field] > value)
+        clauses.append(OPERATORS[operator](cols[field], value))
 
     # Tie filters together using the operator
-    if operator == 'or':
+    if nexus == 'or':
         query = query.filter(or_(*clauses))
     else:
         query = query.filter(and_(*clauses))
